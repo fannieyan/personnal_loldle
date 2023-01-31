33532dc417c89f36d3e628d7613372cdf5f15b6b
fannieyan <fannie.yan@hotmail.fr>
2023-01-31 16:43:03 +0100

    Remove unecessary files generated with init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
 </template>
 
@@ -12,19 +8,10 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-color: #111111;
+  color: #f9f9f9;
+  height: "100%";
 }
 </style>
