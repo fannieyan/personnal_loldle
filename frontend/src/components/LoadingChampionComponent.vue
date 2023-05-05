dbee66fbd69b579829215464d61916be82a3bf81
fannieyan <81410437+fannieyan@users.noreply.github.com>
2023-05-05 17:49:40 +0200

    Create loading page

---
+++ frontend/src/components/LoadingChampionComponent.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import LoaderIcon from "@/components/UI/LoaderIcon.vue";
+
+export default defineComponent({
+  name: "LoadingChampionComponent",
+  components: { LoaderIcon },
+});
+</script>
+
+<template>
+  <div><h3>Un champion un appelé à la barre...</h3></div>
+  <LoaderIcon />
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 32px;
+}
+</style>
