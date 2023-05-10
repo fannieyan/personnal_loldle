22d7492f87d590db6c8c8adb37a1a6c7f689a924
fannieyan <81410437+fannieyan@users.noreply.github.com>
2023-05-10 18:50:21 +0200

    Fix url

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api/champion/random', views.get_random_champion),
     path('api/champion/check', views.check_champion),
     path('api/champion', views.search_champion),
-    re_path(r'^api/champion/(?P<id>[a-zA-Z\' ]+)$', views.champion_details),
+    re_path(r'^api/champion/(?P<id>[a-zA-Z\' \.]+)$', views.champion_details),
 ]
