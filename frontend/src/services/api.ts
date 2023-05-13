a63a3c4c58820321407db29acab6314981b61673
fannieyan <81410437+fannieyan@users.noreply.github.com>
2023-05-13 18:31:37 +0200

    Forgotten return

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 export const getAPIUrl = () => {
   const dev = new URLSearchParams(window.location.search).get("dev");
   if (dev && dev === "true") return "http://127.0.0.1:8000/game/api";
-  else "https://personnal-loldle.vercel.app/game/api";
+  else return "https://personnal-loldle.vercel.app/game/api";
 };
