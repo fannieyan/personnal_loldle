52abc960d5a47289db646b02f6d8f825c8e22b00
fannieyan <81410437+fannieyan@users.noreply.github.com>
2023-05-13 18:22:05 +0200

    Revert

--- backend/personnal_loldle/settings.py
+++ backend/personnal_loldle/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 import os
 import sys
 
-from dotenv import load_dotenv
-load_dotenv()
+if not (os.getenv('PRODUCTION') == "true"):
+    from dotenv import load_dotenv
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
